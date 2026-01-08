70f4548893dce39808a38984149ff06c04faf967
Christopher Helms <cjohnhelms@gmail.com>
2026-01-08 15:10:36 -0600

    fix

--- nvim/lua/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "yaml", "json", "go", "rust", "ocaml", "python" },
-	highlight = { enable = true },
-	incremental_selection = { enable = true },
-	textobjects = { enable = true },
-}
